b23480bb8d36bf8d6752bab7dd2b898ac8bfe6ac
Nathaniel Simon <nathanielkate.simon@evsu.edu.ph>
2022-08-10 21:21:34 +0800

    reverted back to commit-e5d6e35 to remove vuetify integration

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <router-view/>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-}
-</script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import App from './App.vue'
 
-loadFonts()
-
+import './assets/css/style.css'
 import './assets/css/bootstrap.min.css'
 import './assets/js/bootstrap.min.js'
-import './assets/css/style.css'
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).use(router).mount('#app')
